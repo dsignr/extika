3d0a5bf52a8afd4b9cb5097084d2b9b8e128dba4
Neya <dsignr@users.noreply.github.com>
2024-11-19 22:20:59 +0530

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExTika.Mixfile do
   def project do
     [
       app: :extika,
-      version: "0.0.5",
+      version: "0.0.6",
       elixir: "~> 1.1",
       compilers: [:tika | Mix.compilers()],
       deps: deps()
